56207988256ee371889b8ca98e66af92af3a64a0
siddharth-mehra <sid15.mehra@gmail.com>
2025-02-17 23:18:20 +0530

    "Changing the sendEmailVerification"

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,7 +1,7 @@
 import dbConnect from '@/lib/dbConnect';
 import UserModel from '@/model/User';
 import bcrypt from 'bcryptjs';
-import { sendVerificationEmail } from '@/helpers/sendVerificationEmail';
+import { sendVerificationEmail } from '../../../helpers/sendVerificationEmail';
 
 export async function POST(request: Request) {
   await dbConnect();
